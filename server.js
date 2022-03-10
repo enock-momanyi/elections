75c162bc8786a9e460ad57868c16340e133b3bed
Enock Momanyi <enockenm@gmail.com>
2022-03-10 15:14:44 +0300

    Update server.js

--- server.js
+++ server.js
@@ -650,7 +650,14 @@ app.delete('/api/candidate/:id', async (req, res) => {
     }
 })
 
-
+function requireHTTPS(req, res, next) {
+    // The 'x-forwarded-proto' check is for Heroku
+    if (!req.secure && req.get('x-forwarded-proto') !== 'https') {
+        return res.redirect('https://' + req.get('host') + req.url);
+    }
+    next();
+}
+app.use(requireHTTPS)
 app.use(express.static(__dirname + '/dist/elections'));
 app.use(express.static(__dirname + '/dist/prisma'));
 app.get('/*', function(req,res) {res.sendFile(path.join(__dirname+'/dist/elections/index.html'));});
